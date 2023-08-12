4aac8afc2c4178ec99add63a83ae4f3271b0a931
Romit Sutariya <romitsutariya@gmail.com>
2023-08-12 12:29:34 +0530

    Graph-depth first seach in java script

---
+++ temp.js
@@ -0,0 +1,22 @@
+
+const depthFirstPrint=(graph,source)=>{
+    const stack=[source];
+    while(stack.length>0){
+        const current=stack.pop();
+        console.log(current)
+        for(let n of graph[current]){
+            stack.push(n)
+        }
+    }
+}
+
+const graph={
+ a:['c','b'],
+ b:['d'],
+ c:['e'],
+ d:['f'],
+ e:[],
+ f:[]
+}
+
+depthFirstPrint(graph,'a')
