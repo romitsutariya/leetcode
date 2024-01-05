c7f3e5d0b60ffeb7875530354c5607c480c22650
Romit Sutariya <romitsutariya@gmail.com>
2024-01-05 09:32:51 +0530

    Time: 342 ms (5.01%), Space: 45 MB (24.39%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+      for(int j=0;j<nums.length+1;j++){
+          int index=-1;
+          for(int i=0;i<nums.length;i++){
+              if(nums[i]==j){
+                  index=i;
+              }
+          }
+          if(index==-1){
+              return j;
+          }
+      }
+        return -1;
+    }
+}
