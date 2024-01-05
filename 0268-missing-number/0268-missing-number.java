98a587f437a74e855a840cd9dedc0bd3c2b34a57
Romit Sutariya <romitsutariya@gmail.com>
2024-01-05 09:34:20 +0530

    Time: 136 ms (5.01%), Space: 45.2 MB (19.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -5,6 +5,7 @@ class Solution {
           for(int i=0;i<nums.length;i++){
               if(nums[i]==j){
                   index=i;
+                  break;
               }
           }
           if(index==-1){
